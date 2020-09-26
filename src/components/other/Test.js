ba34d7bc51960c6232b7efaad5d4e858b6f74957
Mike <troianello@protonmail.com>
2020-09-26 01:40:47 -0500

    cors

--- src/components/other/Test.js
+++ src/components/other/Test.js
@@ -6,7 +6,9 @@ export default class Test extends Component {
   };
 
   componentDidMount = async () => {
-    let results = await fetch('https://jm-powerhack.azurewebsites.net/', {
+    // let results = await fetch('https://jm-powerhack.azurewebsites.net/', {
+
+    let results = await fetch('http://localhost:3000/', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
